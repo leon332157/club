b649fea4a7b3b50d8aac59ff7029c1851cc000bd
leon332157 <leon332157@sina.com>
2018-03-17 14:34:02 -0400

    .

--- pic_test.py
+++ pic_test.py
@@ -1,13 +1,14 @@
-import pyscreenshot,os,time
-listt=[]
-for x in range(0,100):
-    t1=time.time()
+import pyscreenshot, os, time
+
+listt = []
+for x in range(0, 100):
+    t1 = time.time()
     listt.append(pyscreenshot.grab(childprocess=False))
     print(x)
-    print(time.time()-t1)
-i=0
+    print(time.time() - t1)
+i = 0
 for each in listt:
-    filename=os.getcwd()+'/cache/{}.png'.format(i)
+    filename = os.getcwd() + '/cache/{}.png'.format(i)
     print(filename)
     each.save(filename)
-    i+=1
+    i += 1
