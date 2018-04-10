f901346972b0f9b881fa3bb86264741f65ef820e
leon332157 <leon332157@sina.com>
2018-04-10 00:13:40 -0400

    Rewrite in class

--- change password.py
+++ change password.py
@@ -1,41 +1,46 @@
+#! /usr/bin/env python3
 from tkinter import *
 import tkinter.messagebox as messagebox
 from rot13 import Rot13
 import os
 import base64
 
-rot = Rot13()
 
+class ChangePasswordTool:
+    def __init__(self):
 
-def change_password():
-    new = e1.get()
-    if new == '':
-        messagebox.showerror('Please input password.', 'Please input password.')
+        self.rot = Rot13()
+        root = Tk()
+        self.l1 = Label(root, text='Change Password Tool', font=('', 28))
+        self.l2 = Label(root, text='New password:')
+        self.e1 = Entry(root, show='*')
+        self.b1 = Button(root, text='Change password', command=self.change_password)
+        self.b2 = Button(root, text='exit', command=exit)
+        self.l1.pack()
+        self.l2.pack()
+        self.e1.pack()
+        self.b1.pack()
+        self.b2.pack()
+        try:
+            root.title('Change Password Tool')
+            root.resizable(width=False, height=False)
+            root.mainloop()
+        except KeyboardInterrupt:
+            exit(0)
+
+    def change_password(self):
+        new = self.e1.get()
+        if new == '':
+            messagebox.showerror('Please input password.', 'Please input password.')
+            return
+        try:
+            with open(os.getcwd() + '/password.pc', mode='w+') as f:
+                f.write(base64.b64encode(bytes(self.rot.encodes(new).encode('utf8'))).decode('utf8'))
+                f.close()
+            messagebox.showinfo(title='Successful', message='Successfully saved password, your password is: ' + new)
+        except Exception as e:
+            messagebox.showerror(title='Error', message=e)
         return
-    try:
-        with open(os.getcwd() + '/password.pc', mode='w+') as f:
-            f.write(base64.b64encode(bytes(rot.encodes(new).encode('utf8'))).decode('utf8'))
-            f.close()
-        messagebox.showinfo(title='Successful', message='Successfully saved password, your password is: ' + new)
-    except Exception as e:
-        messagebox.showerror(title='Error', message=e)
-    return
 
 
-root = Tk()
-l1 = Label(root, text='Change Password Tool', font=('', 28))
-l2 = Label(root, text='New password:')
-e1 = Entry(root, show='*')
-b1 = Button(root, text='Change password', command=change_password)
-b2 = Button(root, text='exit', command=exit)
-l1.pack()
-l2.pack()
-e1.pack()
-b1.pack()
-b2.pack()
-try:
-    root.title('Change Password Tool')
-    root.resizable(width=False, height=False)
-    root.mainloop()
-except KeyboardInterrupt:
-    exit(0)
+ChangePasswordTool()
